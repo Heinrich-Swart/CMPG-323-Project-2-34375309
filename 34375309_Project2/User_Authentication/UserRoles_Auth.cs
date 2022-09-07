563cb104228462baaef6f992f6dd9be36bc4ee53
Heinrich-Swart <swartheinrich12@gmail.com>
2022-09-07 03:12:12 +0200

    Wel

--- 34375309_Project2/User_Authentication/UserRoles_Auth.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _34375309_Project2.Authentication
-{
-    public static class UserRoles_Auth
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-    }
-}
-
